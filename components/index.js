360046b773622ad03b590042fa1c5cedc69a79ef
Javier Gomez <javiergomezve@gmail.com>
2022-06-14 22:16:37 -0500

    [ADD] Home screen

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import FocusedStatusBar from './FocusedStatusBar';
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import { CircleButton, RecButton } from './Button';
+
+export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RecButton };
