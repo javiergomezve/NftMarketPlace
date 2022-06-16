96ed634dddf7ef0f53c0287b78f32d99a10690fe
Javier Gomez <javiergomezve@gmail.com>
2022-06-15 21:30:41 -0500

    [ADD] Details page

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import FocusedStatusBar from './FocusedStatusBar';
 import HomeHeader from './HomeHeader';
 import NFTCard from './NFTCard';
 import { CircleButton, RecButton } from './Button';
+import DetailsDescription from './DetailsDescription';
+import DetailsBid from './DetailsBid';
+import { SubInfo } from './SubInfo';
 
-export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RecButton };
+export {
+    FocusedStatusBar,
+    HomeHeader,
+    NFTCard,
+    CircleButton,
+    RecButton,
+    DetailsDescription,
+    DetailsBid,
+    SubInfo,
+};
